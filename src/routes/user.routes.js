1a09a8596446311b022e8506b2253b3b039b20c4
poojitha <poojitharamala@gmail.com>
2025-09-23 11:08:22 +0530

    user controller for password

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -8,7 +8,8 @@ const Users = new UserController()
 
 
 router.post("/user" ,Users.createuser.bind(Users))
-router.get("/getall",Users.getall.bind(Users))
 router.post("/login",Users.loginuser.bind(Users))
+router.get("/getall",Users.getall.bind(Users))
+router.delete("/delete/:id",Users.delete.bind(Users))
 
 module.exports  = router
