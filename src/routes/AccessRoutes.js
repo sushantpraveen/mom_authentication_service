d4ccce68ad2eeebab04e725614e017d3a20508bf
chandu <chandunatakarani@gmail.com>
2025-09-29 17:55:41 +0530

    access controller

--- src/routes/AccessRoutes.js
+++ src/routes/AccessRoutes.js
@@ -8,5 +8,7 @@ const Access= new AccessRoutes()
 //CRUD OPERATIONS
 router.post('/create',Access.CreateUsers.bind(Access))
 router.get('/get',Access.GetAllUsers.bind(Access))
+router.put('/update',Access.UpdateUsers.bind(Access))
+router.put('/remove',Access.removeAccessControl.bind(Access))
 
 module.exports=router
